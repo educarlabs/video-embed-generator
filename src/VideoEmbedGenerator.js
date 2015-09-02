4b9935f049cc8115e101a74d5e06f0ab458ddc5c
Pablo Enrici <penrici@educ.gov.ar>
2015-09-02 14:44:24 -0300

    Commit inicial.

---
+++ src/VideoEmbedGenerator.js
@@ -0,0 +1,170 @@
+/**
+ * Herramienta para generar códigos de embebido de forma interactiva al estilo YouTube
+ *
+ * @author Pablo Enrici
+ */
+
+(function(window, document, undefined){
+
+  var url,
+      code,
+      form,
+      target,
+      widthHeightRegex = /w(\d+)h(\d+)/,
+
+      // Opciones de configuración por defecto
+      defaults = {
+        formSelector  : "[data-videoembedgenerator]",
+        targetSelector: "[data-videoembedgenerator-target]"
+      };
+
+
+  /**
+   * Inicializa el generador de códigos de embebido.
+   */
+  function init(options) {
+
+    // Aplicamos defaults para las opciones no definidas
+    for (var prop in defaults) {
+      if (prop in options) { continue; }
+      options[prop] = defaults[prop];
+    }
+
+    form = document.querySelector(options.formSelector);
+
+    if (null === form) {
+      console.warn("No se encontró un elemento que coincida con el selector '" + options.formSelector + "' \
+                    para inicializar el generador de códigos de embebido.");
+      return;
+    }
+
+    target = document.querySelector(options.targetSelector);
+
+    if (null === target) {
+      console.warn("No se encontró un elemento que coincida con el selector '" + options.targetSelector + "' \
+                    para inicializar el generador de códigos de embebido.");
+      return;
+    }
+
+    target.value = getEmbedCode();
+
+    form.addEventListener("change", refreshCode);
+  }
+
+
+  /**
+   * Destruye la instancia del generador.
+   */
+  function kill() {
+    form.removeEventListener("change", refreshCode);
+    form = target = url = code = null;
+  }
+
+
+  /**
+   * Actualiza el código de embebido en base a la configuración del formulario.
+   *
+   * @param {Event} event - Evento de tipo "change" sobre le formulario.
+   */
+  function refreshCode(event) {
+    updateForm(event.target);
+    target.value = getEmbedCode();
+  }
+
+
+  /**
+   * Actualiza estados y valores del fomulario ante un cambio.
+   *
+   * @param {Element} changedElement - Elemento que disparó el cambio.
+   */
+  function updateForm(changedElement) {
+    switch (changedElement.name) {
+
+      case "autostart":
+        form.elements.start.readOnly = !changedElement.checked;
+        form.elements.start.value    =  changedElement.checked ? 0 : null;
+        break;
+
+      case "autostop":
+        form.elements.stop.readOnly = !changedElement.checked;
+        form.elements.stop.value    =  changedElement.checked ? +form.elements.start.value + 1 : null;
+        break;
+
+      case "dimensiones":
+        switch(form.elements.dimensiones.value) {
+
+          case "none":
+            form.elements.width.value     = null;
+            form.elements.height.value    = null;
+            form.elements.width.readOnly  = true;
+            form.elements.height.readOnly = true;
+            break;
+
+          case "custom":
+            form.elements.width.value     = 320;
+            form.elements.height.value    = 240;
+            form.elements.width.readOnly  = false;
+            form.elements.height.readOnly = false;
+            break;
+
+          default:
+            form.elements.width.readOnly  = true;
+            form.elements.height.readOnly = true;
+
+            var dimsArr = form.elements.dimensiones.value.match(widthHeightRegex);
+            form.elements.width.value  = dimsArr ? dimsArr[1] : null;
+            form.elements.height.value = dimsArr ? dimsArr[2] : null;
+        }
+    }
+  }
+
+
+  /**
+   * Devuelve la URL del servicio de videos.
+   *
+   * @return {string}
+   */
+  function getUrl() {
+    url  = form.elements.baseurl.value;
+    url += "?id=" + form.elements.rec_id.value;
+    url +=  form.elements.cc.checked                  ? "&cc=1"                                       : "";
+    url += !form.elements.info.checked                ? "&info=0"                                     : "";
+    url += !form.elements.controls.checked            ? "&controls=0"                                 : "";
+    url +=  form.elements.autostart.checked           ? "&autostart=1"                                : "";
+    url +=  form.elements.start.value                 ? "&start="     + form.elements.start.value     : "";
+    url +=  form.elements.stop.value                  ? "&stop="      + form.elements.stop.value      : "";
+    url +=  form.elements.skin.value      != "seven"  ? "&skin="      + form.elements.skin.value      : "";
+    url +=  form.elements.referente.value != "educar" ? "&referente=" + form.elements.referente.value : "";
+
+    return url;
+  }
+
+
+  /**
+   * Devuelve el código completo para embeber un video.
+   *
+   * @return {string}
+   */
+  function getEmbedCode() {
+    var rwdOpenWrapper  = '<div style="position: relative; height: 0; padding-top: 56.248%">',
+        rwdCloseWrapper = '</div>',
+        rwdIframeStyles = 'style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"',
+        rwd = (!form.elements.width.value && !form.elements.height.value);
+
+    code  = rwd ? rwdOpenWrapper : '';
+    code += '<iframe frameborder="0" ' + (rwd ? rwdIframeStyles : "") + ' allowfullscreen src="' + getUrl() + '"';
+    code += form.elements.width.value  ? ' width="'  + form.elements.width.value  + '"' : "";
+    code += form.elements.height.value ? ' height="' + form.elements.height.value + '"' : "";
+    code += '></iframe>';
+    code += rwd ? rwdCloseWrapper : '';
+
+    return code;
+  }
+
+  // Exportamos la API al scope global
+  window.VideoEmbedGenerator = {
+    init: init,
+    kill: kill
+  };
+
+})(window, document);
